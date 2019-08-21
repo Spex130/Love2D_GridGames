c6964e80c148d64673966cda8959132a2048019b
Spex130 <Gamerguy56@bellsouth.net>
2019-08-21 13:33:28 -0400

    Init commit of some template style files. Added in WALT animation control.

---
+++ animFile.lua
@@ -0,0 +1,1 @@
+local animator = require 'animator'
---
+++ main.lua
@@ -0,0 +1,13 @@
+local animFile = require "animFile"
+
+function love.update(dt)
+
+end
+
+function love.draw(dt)
+
+end
+
+function love.load(arg)
+
+end
